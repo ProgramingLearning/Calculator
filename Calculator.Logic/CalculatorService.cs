acd0d643993231c8f36dfdf4f6158bdbda15b0a2
Andreea Stani <stani.andreea@gmail.com>
2023-04-28 11:19:24 +0300

    LEAR-15-code_review

--- Calculator.Logic/CalculatorService.cs
+++ Calculator.Logic/CalculatorService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Cache;
+
 using System.Text;
 
 namespace Calculator.Logic
@@ -16,48 +17,44 @@ namespace Calculator.Logic
         {
             _calculatorLogic = calculatorLogic;
         }
-        public CalculatorResponse GetCalculatorResponse(CalculatorRequest request)
+        public CalculatorResponse GetCalculatorResponseForMultipleTermOperation(CalculatorRequest request)
         {
             var calculatorResponse = new CalculatorResponse();
             var strings = request.ButtonClicked.Split('_').ToList();
-
             SetTheCalculatorStateIfExistsInRequest(request);
-            if (strings != null && strings.Count > 0)
-            {
-                _calculatorLogic.AddTerm(strings[0]);
-
-                if (strings[1] == "=")
-                {
-                    calculatorResponse.CalculatorResult = _calculatorLogic.DoMultipleTermOperation();
-                }
-                else
-                {
-                    Operation operation = ParseStringToEnumOperation(strings);
-                    _calculatorLogic.SetCurrentOperation(operation);
-                    switch (operation)
-                    {
-                        case Operation.IsPrime:
-                        case Operation.AbsoluteValue:
-                        case Operation.IsOddOrEven:
-                        case Operation.SquareRoot:
-                        case Operation.IsSuperpalindrome:
-                        case Operation.IsPalindrome:
-                        case Operation.Reverse:
-                        case Operation.ChangeSign:
-                            calculatorResponse.CalculatorResult = _calculatorLogic.DoSingleTermOperation();
-                            break;
-                        default:
-                            calculatorResponse.CalculatorState = _calculatorLogic.GetCalculatorState();
-                            break;
-                    }
-                }
-            }
+            _calculatorLogic.AddTerm(strings[0]);
+            ProcessOperationForMultipleTerm(calculatorResponse, strings[1]);
             return calculatorResponse;
         }
-            private static Operation ParseStringToEnumOperation(List<string> strings)
+
+        public CalculatorResponse GetCalculatorResponseForSingleTermOperation(CalculatorRequest request)
         {
-            var operation = Enum.Parse<Operation>(strings[1]);
-            return operation;
+            var calculatorResponse = new CalculatorResponse();
+            var strings = request.ButtonClicked.Split('_').ToList();
+            _calculatorLogic.AddTerm(strings[0]);
+            ProcessOperationForSingleTerm(calculatorResponse, strings[1]);
+            return calculatorResponse;
+        }
+
+        private void ProcessOperationForMultipleTerm(CalculatorResponse calculatorResponse, string strings)
+        {
+            if (strings == "=")
+            {
+                calculatorResponse.CalculatorResult = _calculatorLogic.DoMultipleTermOperation();
+            }
+            else
+            {
+                var operation = Enum.Parse<Operation>(strings);
+                _calculatorLogic.SetCurrentOperation(operation);
+                calculatorResponse.CalculatorState = _calculatorLogic.GetCalculatorState();
+            }
+        }
+
+        private void ProcessOperationForSingleTerm(CalculatorResponse calculatorResponse, string strings)
+        {
+            var operation = Enum.Parse<Operation>(strings);
+            _calculatorLogic.SetCurrentOperation(operation);
+            calculatorResponse.CalculatorResult = _calculatorLogic.DoSingleTermOperation();
         }
 
         private void SetTheCalculatorStateIfExistsInRequest(CalculatorRequest request)
@@ -69,3 +66,4 @@ namespace Calculator.Logic
         }
     }
 }
+
